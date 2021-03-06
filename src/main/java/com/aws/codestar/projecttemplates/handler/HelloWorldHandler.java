6fea9158258abe2fc2f6bd060bef6df15daff75c
olsongrant <grant.olson@gmail.com>
2021-03-06 00:43:00 +0000

    added public URL connectivity check

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -2,12 +2,16 @@ package com.aws.codestar.projecttemplates.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 import com.aws.codestar.projecttemplates.GatewayResponse;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 /**
  * Handler for requests to Lambda function.
@@ -17,6 +21,23 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
+        Boolean canConnectToPublicURLs = this.checkOutgoingConnectivity(context.getLogger());
+        JSONObject contentObject = new JSONObject();
+        contentObject.put("Output", "Hello World!");
+        contentObject.put("canConnectToPublicURLs", canConnectToPublicURLs);
+        return new GatewayResponse(contentObject.toString(), headers, 200);
+    }
+    
+    private boolean checkOutgoingConnectivity(LambdaLogger logger) {
+        try {
+            URL urlObj = new URL("https://jsonplaceholder.typicode.com/users");
+            HttpURLConnection httpCon = (HttpURLConnection) urlObj.openConnection();
+            int responseCode = httpCon.getResponseCode();
+            logger.log("response code from URL check in checkOutgoingConnectivity: " + responseCode);
+            return (responseCode > 0) ? true : false;
+        } catch (Exception e) {
+            logger.log(e.getMessage());
+            return false;
+        }
     }
 }
